7c8c3f4367642346531bbba6123f6add92cc6fc2
Isaac Bonilla <103343652+yzac-bonilla@users.noreply.github.com>
2022-04-09 21:37:50 -0500

    Add files via upload

---
+++ 8vo semestre/Software Development/Practicas/P1 - Lanzador de Firma/Applicacion/Applicacion/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Applicacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Firma = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Firma
+            // 
+            this.Firma.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Firma.Location = new System.Drawing.Point(82, 49);
+            this.Firma.Name = "Firma";
+            this.Firma.Size = new System.Drawing.Size(75, 23);
+            this.Firma.TabIndex = 0;
+            this.Firma.Text = "Firma";
+            this.Firma.UseVisualStyleBackColor = false;
+            this.Firma.Click += new System.EventHandler(this.Firma_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkCyan;
+            this.ClientSize = new System.Drawing.Size(245, 110);
+            this.Controls.Add(this.Firma);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Lanzador de Firma";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Firma;
+    }
+}
+
