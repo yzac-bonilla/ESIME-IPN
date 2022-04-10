407ff816af01548e76b031633cef6f6a10b27dc3
Isaac Bonilla <103343652+yzac-bonilla@users.noreply.github.com>
2022-04-09 21:49:05 -0500

    Add files via upload

---
+++ 8vo semestre/Software Development/Practicas/P20 - Graficador Coseno/Graficador/Graficador/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using info.lundin.math;
+
+namespace Graficador
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            double xi = Double.Parse(textBox1.Text);
+            double xf= Double.Parse(textBox2.Text);
+            Graficas gra = new Graficas();
+            gra.GraficadorXY(g, xi, xf);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"Bonilla.exe");
+        }
+    }
+}
---
+++ 8vo semestre/Software Development/Practicas/P20 - Graficador Coseno/Graficador/Graficador/Graficas.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Graficador
+{
+    class Graficas
+    {
+        private double x, y, xi, yi, xf, yf;
+        private int c, f, ci, fi, cf, ff, n;
+        public Graficas() { }
+        public void GraficadorXY(Graphics g, double xi,double xf)
+        {
+            int c1, f1;
+            double h;
+            ci = 1;
+            cf = 300;
+            fi = 1;
+            ff = 300;
+            this.xi = xi;
+            this.xf = xf;
+            n = cf - ci;
+            h = (xf - xi) / n;
+            yi = yf = 0;
+            for(int k=1;k< n;k++)
+            {
+                x = xi + k * h;
+                y = fu(x);
+                if (y < yi) yi = y;
+                if (y > yf) yf = y;
+            }
+            x = xi;
+            c1 =(int) col(x);
+            y = fu(x);
+            f1 = (int)fil(y);
+            for(int k=1;k< n;k++)
+            {
+                x = xi + k * h;
+                y = fu(x);
+                c =(int) col(x);
+                f = (int) fil(y);
+
+                g.DrawLine(Pens.Blue, c1, f1, c, f);
+                c1 = c;
+                f1 = f;
+            }
+        }
+        public double fu(double x)
+        {
+            double ys;
+            ys = Math.Cos(x);
+            return ys;
+        }
+        public double col(double x)
+        {
+            double c = ((xf - x) * ci) / (xf - xi) + ((x - xi) * cf) / (xf - xi);
+            return c;
+        }
+
+        public double fil(double y)
+        {
+            double f = ((yf - y) * ff) / (yf - yi) + ((y - yi) * fi) / (yf - yi);
+            return f;
+        }
+
+
+    }
+}
