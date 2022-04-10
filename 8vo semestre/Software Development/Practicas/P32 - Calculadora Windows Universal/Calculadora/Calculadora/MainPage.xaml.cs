5abd7f461b88d3e2ad77b2490629f8beecc28670
Isaac Bonilla <103343652+yzac-bonilla@users.noreply.github.com>
2022-04-09 22:38:24 -0500

    Add files via upload

---
+++ 8vo semestre/Software Development/Practicas/P32 - Calculadora Windows Universal/Calculadora/Calculadora/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Calculadora
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        double x, y, z;
+
+        private void button_Copy1_Click(object sender, RoutedEventArgs e)
+        {
+            x = double.Parse(textBox1.Text);
+            y = double.Parse(textBox2.Text);
+            z = x - y;
+            listBox.Items.Add(z.ToString());
+        }
+
+        private void button_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            x = double.Parse(textBox1.Text);
+            y = double.Parse(textBox2.Text);
+            z = x * y;
+            listBox.Items.Add(z.ToString());
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            x = double.Parse(textBox1.Text);
+            y = double.Parse(textBox2.Text);
+            z = x / y;
+            listBox.Items.Add(z.ToString());
+        }
+
+        private void textBlock1_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            x = double.Parse(textBox1.Text);
+            y = double.Parse(textBox2.Text);
+            z = x + y;
+            listBox.Items.Add(z.ToString());
+        }
+    }
+}
