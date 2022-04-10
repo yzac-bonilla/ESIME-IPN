0784c0e943dc9cac7decfc01dc60148d440c433d
Isaac Bonilla <103343652+yzac-bonilla@users.noreply.github.com>
2022-04-09 21:47:39 -0500

    Add files via upload

---
+++ 8vo semestre/Software Development/Practicas/P18 - Dibujar figuras con Mouse/Figuras_Eventos/Figuras_Eventos/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Figuras_Eventos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Punto";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Linea";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(174, 21);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Rectangulo";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(255, 21);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Circulo";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(467, 21);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Firma";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 61);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(530, 380);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(336, 21);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Limpiar";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Figuras_Eventos.Properties.Resources.oax;
+            this.ClientSize = new System.Drawing.Size(554, 453);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Figuras - Evento Mouse";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ 8vo semestre/Software Development/Practicas/P18 - Dibujar figuras con Mouse/Figuras_Eventos/Figuras_Eventos/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Figuras_Eventos
+{
+    public partial class Form1 : Form
+    {
+        bool punto, linea, rectangulo, circulo, click=true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            punto = true;
+            linea = rectangulo = circulo = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            linea = true;
+            punto = rectangulo = circulo = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            rectangulo = true;
+            punto = linea = circulo = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            circulo = true;
+            punto = linea = rectangulo = false;
+        }
+
+        int x1, y1, x2, y2;
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Graphics obj_gra = pictureBox1.CreateGraphics();
+            Rectangle obj_rect = new Rectangle();
+
+            if (click)
+            {
+                x1 = e.X;
+                y1 = e.Y;
+                click=false;
+
+                if (punto)
+                {
+                    obj_gra.DrawLine(Pens.Blue, x1, y1, x1 + 1, y1);
+                    click=true;
+                }
+            }
+            else
+            {
+                x2 = e.X;
+                y2 = e.Y;
+                click = true;
+
+                
+                if (x2 > x1)
+                {
+                    obj_rect.X = x1;
+                    obj_rect.Width = x2 - x1;
+                }
+                else
+                {
+                    obj_rect.X = x2;
+                    obj_rect.Width = x1 - x2;
+                }
+
+                if (y2 > y1)
+                {
+                    obj_rect.Y = y1;
+                    obj_rect.Height = y2 - y1;
+                }
+                else
+                {
+                    obj_rect.Y = y2;
+                    obj_rect.Height = y1 - y2;
+                }
+
+                if (linea)
+                {
+                    obj_gra.DrawLine(Pens.Red, x1, y1, x2 + 1, y2);
+                }
+                else if (rectangulo)
+                {
+                    obj_gra.DrawRectangle(Pens.Green, obj_rect);
+                }
+                else if (circulo)
+                {
+                    obj_gra.DrawEllipse(Pens.Fuchsia, obj_rect);
+                }
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+                System.Diagnostics.Process.Start(@"Bonilla.exe");
+            
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+        }
+
+    }
+}
