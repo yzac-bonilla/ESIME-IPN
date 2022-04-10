7899e91ee5608d185f49a1283de1b6b28b57eb4e
Isaac Bonilla <103343652+yzac-bonilla@users.noreply.github.com>
2022-04-09 21:39:48 -0500

    Add files via upload

---
+++ 8vo semestre/Software Development/Practicas/P4 - Lanzador de Form/Lanzar_Formulario/Lanzar_Formulario/Form2.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Lanzar_Formulario
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Firma = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(165, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Soy form 2";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Firma
+            // 
+            this.Firma.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Firma.Location = new System.Drawing.Point(185, 216);
+            this.Firma.Name = "Firma";
+            this.Firma.Size = new System.Drawing.Size(75, 23);
+            this.Firma.TabIndex = 1;
+            this.Firma.Text = "Firma";
+            this.Firma.UseVisualStyleBackColor = false;
+            this.Firma.Click += new System.EventHandler(this.Firma_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOrange;
+            this.BackgroundImage = global::Lanzar_Formulario.Properties.Resources.Chrysanthemum;
+            this.ClientSize = new System.Drawing.Size(431, 261);
+            this.Controls.Add(this.Firma);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Firma;
+    }
+}
