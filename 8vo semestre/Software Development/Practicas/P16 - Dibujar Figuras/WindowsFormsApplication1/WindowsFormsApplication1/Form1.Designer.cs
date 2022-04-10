43149ff026798e8b085ea2623ff15e10a5b2cce8
Isaac Bonilla <103343652+yzac-bonilla@users.noreply.github.com>
2022-04-09 21:46:36 -0500

    Add files via upload

---
+++ 8vo semestre/Software Development/Practicas/P16 - Dibujar Figuras/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.PB1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PB1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Puntos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // PB1
+            // 
+            this.PB1.BackColor = System.Drawing.Color.White;
+            this.PB1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.PB1.Location = new System.Drawing.Point(12, 41);
+            this.PB1.Name = "PB1";
+            this.PB1.Size = new System.Drawing.Size(516, 319);
+            this.PB1.TabIndex = 1;
+            this.PB1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(174, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Rectangulos";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(93, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Lineas";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(255, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Circulos";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(453, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Firma";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(336, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Limpiar";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources.bcs;
+            this.ClientSize = new System.Drawing.Size(543, 372);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.PB1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Dibujar Figuras";
+            ((System.ComponentModel.ISupportInitialize)(this.PB1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox PB1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ 8vo semestre/Software Development/Practicas/P16 - Dibujar Figuras/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics q = PB1.CreateGraphics();
+            Random rnd = new Random();
+            int x, y, r, g, b;
+            Pen lapiz = new Pen(Color.White);
+            for(int k=1;k<1000;k++)
+            {
+                x = rnd.Next(PB1.Width);
+                y = rnd.Next(PB1.Height);
+                r = rnd.Next(255);
+                g = rnd.Next(255);
+                b = rnd.Next(255);
+                lapiz = new Pen(Color.FromArgb(100,r,g,b));
+                q.DrawLine(lapiz, x, y, x + 1, y);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Graphics q = PB1.CreateGraphics();
+            Random rnd = new Random();
+            int x, y, r, g, b;
+            Pen lapiz = new Pen(Color.White);
+            for (int k = 1; k < 10; k++)
+            {
+                x = rnd.Next(PB1.Width);
+                y = rnd.Next(PB1.Height);
+                r = rnd.Next(255);
+                g = rnd.Next(255);
+                b = rnd.Next(255);
+                lapiz = new Pen(Color.FromArgb(100, r, g, b));
+                Point[] points =
+                {
+                    new Point(x,  y),
+                    new Point(10, 100),
+                    new Point(200,  50),
+                    new Point(250, 300)
+                 };
+                q.DrawLines(lapiz, points);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Graphics q = PB1.CreateGraphics();
+            Random rnd = new Random();
+            int x, y, r, g, b;
+            Pen lapiz = new Pen(Color.White);
+            for (int k = 1; k < 10; k++)
+            {
+                x = rnd.Next(PB1.Width);
+                y = rnd.Next(PB1.Height);
+                r = rnd.Next(255);
+                g = rnd.Next(255);
+                b = rnd.Next(255);
+                lapiz = new Pen(Color.FromArgb(100, r, g, b));
+                RectangleF[] rects =
+                {
+                     new RectangleF( x, y, x, y),
+                     new RectangleF(100.0F, 200.0F, 250.0F,  50.0F),
+                     new RectangleF(300.0F,   0.0F,  50.0F, 100.0F)
+                };
+                q.DrawRectangles(lapiz,rects);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Graphics q = PB1.CreateGraphics();
+            Random rnd = new Random();
+            int x, y, r, g, b;
+            Pen lapiz = new Pen(Color.White);
+            for (int k = 1; k < 10; k++)
+            {
+                x = rnd.Next(PB1.Width);
+                y = rnd.Next(PB1.Height);
+                r = rnd.Next(255);
+                g = rnd.Next(255);
+                b = rnd.Next(255);
+                lapiz = new Pen(Color.FromArgb(100, r, g, b));
+                q.DrawEllipse(lapiz, x, y, x,y);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"Bonilla.exe");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            PB1.Refresh();
+        }
+    }
+}
