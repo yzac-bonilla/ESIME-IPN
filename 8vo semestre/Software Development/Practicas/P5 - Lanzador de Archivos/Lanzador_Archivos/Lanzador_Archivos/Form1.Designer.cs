fdaf5becaa36538fd00ac4270436e7aef4c5590a
Isaac Bonilla <103343652+yzac-bonilla@users.noreply.github.com>
2022-04-09 21:40:42 -0500

    Add files via upload

---
+++ 8vo semestre/Software Development/Practicas/P5 - Lanzador de Archivos/Lanzador_Archivos/Lanzador_Archivos/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Lanzador_Archivos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Firma = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(139, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Lanzar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Firma
+            // 
+            this.Firma.BackColor = System.Drawing.SystemColors.Info;
+            this.Firma.Location = new System.Drawing.Point(253, 165);
+            this.Firma.Name = "Firma";
+            this.Firma.Size = new System.Drawing.Size(75, 23);
+            this.Firma.TabIndex = 1;
+            this.Firma.Text = "Firma";
+            this.Firma.UseVisualStyleBackColor = false;
+            this.Firma.Click += new System.EventHandler(this.Firma_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.BackgroundImage = global::Lanzador_Archivos.Properties.Resources.Chrysanthemum;
+            this.ClientSize = new System.Drawing.Size(338, 198);
+            this.Controls.Add(this.Firma);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Lanzador de archivos";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Firma;
+    }
+}
+
